18fab685ce8ec4edf256c502a688fe4d64f51deb
Ivan Korobov <ivankor48@gmail.com>
2023-12-15 17:56:22 +0300

    Решение задания Практическая Задача 1.1.2. Работа с базой данных c помощью JDBC.

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,94 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
+    private static Statement statement;
+    private static Connection connection;
 
+    static  {
+        connection = Util.getConnection();
+        try {
+            statement = connection.createStatement();
+            statement.execute("CREATE DATABASE IF NOT EXISTS mydatabase");
+            statement.execute("USE mydatabase");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Connection getConnection() {
+        return connection;
     }
 
     public void createUsersTable() {
-
+        try {
+            statement.execute("""
+                              CREATE TABLE IF NOT EXISTS user(
+                                     id SERIAL PRIMARY KEY,
+                                     name VARCHAR(50),
+                                     lastName VARCHAR(50),
+                                     age TINYINT);
+                              """);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void dropUsersTable() {
-
+        try {
+            statement.execute("DROP TABLE IF EXISTS user;");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        try {
+            String sql = String.format("INSERT user(name, lastName, age) VALUES ('%1$s', '%2$s', %3$s);"
+                    ,name,lastName,age);
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void removeUserById(long id) {
-
+        try {
+            statement.executeUpdate("DELETE FROM user WHERE (id = " + id + ");");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        try {
+            ResultSet resultSet = statement.executeQuery("SELECT name, lastName, age FROM User;");
+            List<User> user = new ArrayList<>();
+            while (resultSet.next()){
+                user.add(new User(resultSet.getString("name"),
+                                resultSet.getString("lastName"),
+                                resultSet.getByte("age")));
+            }
+            return user;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void cleanUsersTable() {
-
+        try {
+            statement.executeUpdate("DELETE FROM user;");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,35 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String USERNAME = "Iv";
+    private static final String PASSWORD = "i1234567";
+    private static final String HOSTNAME = "localhost";
+    private static String dbName = "mydatabase";
+    private Util(){
+    }
+
+    public static Connection getConnection(){
+        // Declare the class Driver for MySQL DB
+        // This is necessary with Java 5 (or older)
+        // Java6 (or newer) automatically find the appropriate driver.
+        // If you use Java> 5, then this line is not needed.
+//        try {
+//            Class.forName("com.mysql.jdbc.Driver");
+//        } catch (ClassNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
+
+        String connectionURL = "jdbc:mysql://" + HOSTNAME + ":3306/";
+//        String connectionURL = "jdbc:mysql://" + HOSTNAME + ":3306/" + dbName;
+        try {
+            return DriverManager.getConnection(connectionURL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
